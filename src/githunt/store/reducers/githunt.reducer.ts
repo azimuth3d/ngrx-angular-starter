e8280abdce7977f1ff6aef7f940a75797246a1ec
Khomkrit <azimuth3d@gmail.com>
2018-06-20 17:16:39 +0700

    Update to new account

--- src/githunt/store/reducers/githunt.reducer.ts
+++ src/githunt/store/reducers/githunt.reducer.ts
@@ -1,8 +1,61 @@
-import * as fromActions from './githunt.reducer';
+import * as fromActions from '../actions';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector
+} from '@ngrx/store';
+import { SearchResults } from '../../models';
 
-describe('All githunt module actions perform correctly', () => {
-    test('Search repos reducer work correctly', () => {
-      expect(true).toBeTruthy();
-    }
-  });
-});
+// Default data / initial state
+
+// Reducer
+export interface SearchState {
+  results?: SearchResults;
+  loaded: boolean | false;
+  loading: boolean | false;
+}
+
+const initialState = {
+  results: {},
+  loaded: false,
+  loading: false
+};
+
+export function searchReducer(
+  state: SearchState = initialState,
+  action: fromActions.SearchRepoActions
+) {
+  switch (action.type) {
+    case fromActions.GET_SEARCH_REPOS_SUCCESS:
+      return { ...state, results: action.results };
+    default:
+      return state;
+  }
+}
+
+export interface State {
+  searchItems: SearchState;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  searchItems: searchReducer
+};
+
+// Create the default selectors
+
+export const getSearchs = createFeatureSelector<State>('searchs');
+
+export const getSearchsState = createSelector(
+  getSearchs,
+  (state: State) => state.searchItems
+);
+
+export const getSearchResults = createSelector(
+  getSearchsState,
+  (state: SearchState) => state.results
+);
+
+export const getSearchItems = createSelector(
+  getSearchsState,
+  (state: SearchState) => state.results.items
+);
